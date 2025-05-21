d4523b7fde2c5716233ea50ea4cb7747b0fa8332
Giomit99 <omitkov999@gmail.com>
2025-05-21 22:25:00 +0200

    Modify pom

---
+++ src/test/java/com/biblioteca/model/BookTest.java
@@ -0,0 +1,36 @@
+package com.biblioteca.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookTest {
+
+    @Test
+    public void testBookCreation() {
+        Book book = new Book("Il nome della rosa");
+        assertEquals("Il nome della rosa", book.getTitle());
+        assertFalse(book.isLoaned());
+    }
+
+    @Test
+    public void testSetLoaned() {
+        Book book = new Book("1984");
+        book.setLoaned(true);
+        assertTrue(book.isLoaned());
+    }
+
+    @Test
+    public void testReservationQueue() {
+        Book book = new Book("La coscienza di Zeno");
+        User user1 = new User("Mario", "mario", "mario");
+        User user2 = new User("Luca", "luca", "luca");
+
+        book.addReservation(user1);
+        book.addReservation(user2);
+
+        assertTrue(book.hasReservations());
+        assertEquals(user1, book.getNextReservation());
+        assertEquals(user2, book.getNextReservation());
+        assertFalse(book.hasReservations());
+    }
+}
---
+++ src/test/java/com/biblioteca/service/LoanServiceTest.java
@@ -0,0 +1,60 @@
+package com.biblioteca.service;
+
+import com.biblioteca.controller.LoanController;
+import com.biblioteca.model.Book;
+import com.biblioteca.model.Loan;
+import com.biblioteca.model.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoanServiceTest {
+
+    private class DummyController extends LoanController {
+        @Override
+        public void loanProcessed(Loan loan) {
+        }
+    }
+
+    @Test
+    public void testLoanWhenUserAlreadyHasBook() {
+        DummyController controller = new DummyController();
+        LoanService service = new LoanService(controller);
+
+        Book book = new Book("1984");
+        User user = new User("Anna", "anna", "anna");
+
+        Loan loan = new Loan(book, user);
+        service.processLoan(loan);
+        service.processLoan(loan);
+    }
+
+    @Test
+    public void testLoanWhenBookAlreadyLoanedByOthers() {
+        DummyController controller = new DummyController();
+        LoanService service = new LoanService(controller);
+
+        Book book = new Book("Brave New World");
+        book.setLoaned(true);
+
+        User user = new User("Luca", "luca", "luca");
+        Loan loan = new Loan(book, user);
+
+        service.processLoan(loan);
+        assertTrue(book.hasReservations());
+    }
+
+    @Test
+    public void testLoanWhenBookIsAvailable() {
+        DummyController controller = new DummyController();
+        LoanService service = new LoanService(controller);
+
+        Book book = new Book("Siddhartha");
+        User user = new User("Marco", "marco", "marco");
+        Loan loan = new Loan(book, user);
+
+        service.processLoan(loan);
+
+        assertTrue(book.isLoaned());
+    }
+}
